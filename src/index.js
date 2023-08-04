144b3b531fabf1529c6b8bdbb69e9bde44c837f2
Thuan Nguyen <thuan.nguuyen@yoot.vn>
2023-08-04 15:35:43 +0700

    add for class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 //import helloWorld from "./hello-world";
-import addImage from "./add-image";
 import HelloWorldButton from "./components/hello-world-button/hello-world-button";
 
 
-
-helloWorld();
-addImage();
+const HelloWorldButtonBtn = new HelloWorldButton();
+HelloWorldButtonBtn.render(); 
