69e09a50fd182e9314ec5ea38a67534fce2edc15
Thuan Nguyen <thuan.nguuyen@yoot.vn>
2023-08-04 18:38:33 +0700

    clean webpak plgin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 //import helloWorld from "./hello-world";
 import HelloWorldButton from "./components/hello-world-button/hello-world-button";
+import Heading from "./components/heading/heading";
+
+
+const heading = new Heading();
+
+heading.render();
 
 
 const HelloWorldButtonBtn = new HelloWorldButton();
 HelloWorldButtonBtn.render(); 
+
+let ten = 10;
