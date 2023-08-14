4dc6a873c28bfd10f5c9d627db4323f2c6fdf708
Thuan Nguyen <thuan.nguuyen@yoot.vn>
2023-08-14 10:09:07 +0700

    mulyiple pages

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,5 +1,16 @@
-function helloWorld() {
-    console.log("hello world");
-}
+//import helloWorld from "./hello-world";
+import HelloWorldButton from "./components/hello-world-button/hello-world-button";
+import Heading from "./components/heading/heading";
 
-export default helloWorld;
+
+const heading = new Heading();
+
+heading.render();
+
+
+const HelloWorldButtonBtn = new HelloWorldButton();
+HelloWorldButtonBtn.render(); 
+
+let ten = 10;
+
+
