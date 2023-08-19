d114f904a4bf41a02e5afe2d53864059a5390712
Thuan Nguyen <thuan.nguuyen@yoot.vn>
2023-08-19 11:04:08 +0700

    split in to 2 application

--- hello-world/webpack.dev.config.js
+++ hello-world/webpack.dev.config.js
@@ -3,10 +3,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        'hello-world': './src/hello-world.js',
-        'kiwi': './src/kiwi.js'
-    },
+    entry: './src/hello-world.js',
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, './dist'),
@@ -14,7 +11,7 @@ module.exports = {
     },
     mode: 'development',
     devServer: {
-        port: 9000,
+        port: 9001,
         static: {
             directory: path.resolve(__dirname, './dist'),
         },
@@ -25,18 +22,6 @@ module.exports = {
     },
     module: {
         rules: [
-            {
-                test: /\.(png|jpg)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader', 'css-loader'
-                ]
-            },
             {
                 test: /\.scss$/,
                 use: [
@@ -66,17 +51,10 @@ module.exports = {
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             filename: 'hello-world.html',
-            chunks: ['hello-world'],
             title: 'Hello world',
             description: 'Hello world',
             template: 'src/page-template.hbs'
         }),
-        new HtmlWebpackPlugin({
-            filename: 'kiwi.html',
-            chunks: ['kiwi'],
-            title: 'Kiwi',
-            description: 'Kiwi',
-            template: 'src/page-template.hbs'
-        })
+       
     ]
 };
