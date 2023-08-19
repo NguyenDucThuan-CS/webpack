93ce881cebee5a1f317111bb8bb83620edf431f2
Thuan Nguyen <thuan.nguuyen@yoot.vn>
2023-08-19 11:37:41 +0700

    set up mododule federation

--- kiwi/webpack.dev.config.js
+++ kiwi/webpack.dev.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } =  require('webpack').container
+
 
 module.exports = {
     entry:'./src/kiwi.js',
@@ -57,10 +59,17 @@ module.exports = {
        
         new HtmlWebpackPlugin({
             filename: 'kiwi.html',
-           
             title: 'Kiwi',
             description: 'Kiwi',
             template: 'src/page-template.hbs'
+        }),
+        new ModuleFederationPlugin({
+            name: 'KiwiApp',
+            remotes: {
+                HelloWorldApp: 'HelloWorldApp@http://localhost:9001/remoteEntry.js'
+            }
         })
-    ]
+
+
+    ]  
 };
